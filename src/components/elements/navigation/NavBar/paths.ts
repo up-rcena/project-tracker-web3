9dd6ac8f9937b1d3482a0bb0d834e8888812761c
iamroms <rcena@startechup.com>
2022-09-22 09:31:53 +0800

    template

---
+++ src/components/elements/navigation/NavBar/paths.ts
@@ -0,0 +1,45 @@
+import { ISubNav } from '../SubNav/SubNav';
+
+const NAV_LINKS: ISubNav[] = [
+  { label: 'Home', href: '/' },
+  // {  //  Transaction
+  //   label: 'Transactions',
+  //   href: '/transactions',
+  // }, // End transaction
+  // { // Transfer 
+  //   label: 'Transfers',
+  //   href: '/transfers',
+  //   children: [
+  //     {
+  //       label: 'SPL',
+  //       subLabel: 'Get your SPL transfers',
+  //       href: '/transfers/spl',
+  //       logo: 'token',
+  //     },
+  //     {
+  //       label: 'NFT',
+  //       subLabel: 'Get your NFT transfers',
+  //       href: '/transfers/nft',
+  //       logo: 'lazyNft',
+  //     },
+  //   ],
+  // }, // End Transfer
+  {
+    label: 'Balances',
+    href: '/balances',
+    children: [
+      {
+        label: 'NFT',
+        subLabel: 'Get your NFT balances',
+        href: '/balances/nft',
+        logo: 'pack',
+      },
+    ],
+  },
+  {
+    label: 'NFT Minter',
+    href: '/nftMinter',
+  },
+];
+
+export default NAV_LINKS;
---
+++ src/components/templates/home/Home.tsx
@@ -0,0 +1,14 @@
+import { CheckCircleIcon, SettingsIcon } from '@chakra-ui/icons';
+import { Heading, VStack, List, ListIcon, ListItem } from '@chakra-ui/react';
+
+const Home = () => {
+  return (
+    <VStack w={'full'}>
+      <Heading size="md" marginBottom={6}>
+        Web3
+      </Heading>
+    </VStack>
+  );
+};
+
+export default Home;
